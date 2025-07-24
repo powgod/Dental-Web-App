bd85911dc8ff26801d6c5dcdf62f330304366424
abdelhak tbatou <88512653+powgod@users.noreply.github.com>
2025-07-24 16:41:01 +0200

    Delete Dental-Web-App-main/Dental-Web-App-main/web app clincs directory

--- Dental-Web-App-main/Dental-Web-App-main/web app clincs/assets/js/common.js
+++
@@ -1,7 +0,0 @@
-function saveToStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
-
-function loadFromStorage(key) {
-  return JSON.parse(localStorage.getItem(key) || '[]');
-}
